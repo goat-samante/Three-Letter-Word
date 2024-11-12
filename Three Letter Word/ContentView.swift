e45a34c76514b40669224fc3340ddccc51a8e5c7
Samuel Amante <amantsa76@gmail.com>
2024-11-12 09:44:50 -0600

    Initial Commit

---
+++ Three Letter Word/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Three Letter Word
+//
+//  Created by Samuel Amante on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
